32f3ac1507c5b4b41fab518eb4bd424562be1a58
anurag-221 <anuragsrivastava696@gmail.com>
2025-09-07 02:15:26 +0530

    test

--- tests/test_daily_issue_helper.py
+++ tests/test_daily_issue_helper.py
@@ -1,8 +1,16 @@
+# tests/test_daily_issue_helper.py
 from daily_issue_helper import get_today_day
 from datetime import date
+import sys
+import os
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..")
+    )
+)
 
 
 def test_get_today_day():
     start = date(2025, 1, 1)
-    today = date(2025, 1, 3)
-    assert get_today_day(start, today) == 3
+    assert get_today_day(start) == 3
