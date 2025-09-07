758b229ae117b6bee8cf103d77212a1b7fb85b4e
anurag-221 <anuragsrivastava696@gmail.com>
2025-09-07 05:39:25 +0530

    calcultor

---
+++ tests/Training/calculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+
+operator = input("Enter operator (+, -, *, /) : ")
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+else:
+    result = "Invalid operator"    
+
+print(f"Result:, {result}")
